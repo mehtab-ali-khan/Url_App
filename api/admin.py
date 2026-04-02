564a454e390d9626caa095c4e82b1eb3928ccb3e
Mehtab <mehtab36143101@gmail.com>
2026-04-02 15:14:12 +0500

    register modals for admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Url, UrlPing
+
+
+@admin.register(Url)
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ["url", "created_at"]
+    search_fields = ["url"]
+
+
+@admin.register(UrlPing)
+class UrlPingAdmin(admin.ModelAdmin):
+    list_display = ["url", "status_code", "time", "error_snapshot"]
+    list_filter = ["status_code"]
+    search_fields = ["url__url"]
